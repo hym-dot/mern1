78bdb89a604763d7c628e6be3c19080e21d7375e
hym-dot <yumin6350@kbu.ac.kr>
2025-08-13 11:39:50 +0900

    。

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -11,47 +11,54 @@ router.post("/", async (req, res) => {
         res.status(201).json(saved)
 
     } catch (error) {
-        res.status(400).json({message:'작성실패', error})
+        res.status(400).json({ message: '작성실패', error })
     }
 })
+
 
 router.get("/", async (req, res) => {
     try {
-        const posts = await Post.find().sort({createdAt:-1})
+        const posts = await Post.find().sort({ createdAt: -1 })
 
-        res.status(201).json(saved)
+        res.status(201).json(posts)
 
     } catch (error) {
-        res.status(400).json({message:'불러오기 실패', error})
+        res.status(400).json({ message: '불러오기 실패', error })
     }
 })
+
 
 router.get("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id)
 
+        if (!post) return res.status(404).json({ message: '수정할 글 없음' })
+
         res.status(201).json(post)
 
     } catch (error) {
-        res.status(400).json({message:'1개 불러오기 실패', error})
+
+        res.status(400).json({ message: '1개 불러오기 실패', error })
     }
 })
 
 router.put("/:id", async (req, res) => {
     try {
-        const post = await Post.findByIdAndUpdate(
+
+        const updated = await Post.findByIdAndUpdate(
             req.params.id,
             req.body,
             {
-                new:true
+                new: true
             }
         )
-        if(!updated) return res.status(404).json({message:'수정할 글 없음'})
+
+        if (!updated) return res.status(404).json({ message: '수정할 글 없음' })
 
         res.status(201).json(updated)
 
     } catch (error) {
-        res.status(400).json({message:'1개 불러오기 실패', error})
+        res.status(400).json({ message: '1개 불러오기 실패', error })
     }
 })
 
